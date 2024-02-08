747ad771fdaa102e6e8fd8cb1e83099c5a01f947
cmatson1312 <156477823+cmatson1312@users.noreply.github.com>
2024-02-07 23:39:33 -0700

    Update code.js

--- code.js
+++ code.js
@@ -8,41 +8,8 @@
 function Function() {
     setTimeout(setAlert, 2000);
 }
-function setAlert() {
-  alert("Assignment 3.1");
+function Function(g1, g2) {
+	return g1 / g2;
 }
-function play() {
-var die1 =Math.ceil(Math.random()*6);
-
-var die2 =Math.ceil(Math.random()*6);
-
-var sum = die1+die2
-
-
-document.write("Die 1 = " + die1) 
-document.write("<br/>") 
-document.write("Die 2 = " + die2) 
-document.write("<br/>") 
-document.write("Sum = " + sum)
-document.write("<br/>") 
-if (sum == 7 || sum == 11) 
-{ 
-
-document.write("CRAPS - you lose") 
-document.write("<br/>") 
-}
-else if (die1== die2 && die1%2 == 0)
-{
-document.write("DOUBLES - you win") 
-document.write("<br/>") 
-}
-
-}
-
-
-and in the html
-
-document.write("Simple Craps")
-document.write("<br/>") 
-
-play();
+const value = myFunction(8, 2);
+console.log(value);
